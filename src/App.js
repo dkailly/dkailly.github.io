d5922b7f4e4417eef0eff54cde5f5ca4ca3c3e1c
Dan <daniel.kailly@gmail.com>
2021-02-15 16:38:49 -0800

    put up site + links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [showContactInfo, setShowContactInfo] = useState(false);
+
+  const toggleContactInfo = useCallback(() => {
+    setShowContactInfo((current) => !current);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          paddingLeft: "12px",
+          paddingRight: "12px",
+        }}
+      >
+        <div className="card">
+          {showContactInfo ? (
+            <div className="App-test">
+              <h1>Contact</h1>
+              <h2>
+                <a href="mailto:daniel.kailly@gmail.com">Email </a>
+              </h2>
+              <h2>
+                <a href="https://www.linkedin.com/in/dkailly/">LinkedIn</a>
+              </h2>
+            </div>
+          ) : (
+            <>
+              <h1>Daniel Kailly</h1>
+              <h2>Vancouver Software Developer</h2>
+            </>
+          )}
+          <button onClick={toggleContactInfo}>
+            {showContactInfo ? "About" : "Contact"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
